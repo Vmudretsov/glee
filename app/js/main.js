95712d8dbeafaf774bb93065c7b6e277de3ac50d
Vmudretsov <mr.mudretsov@mail.ru>
2021-05-06 07:19:04 +0700

    added product-list1 page and fix bugs

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,32 @@
 $(function() {
 
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $('.filter-price__from').text('$' + data.from + '.00');
+      $('.filter-price__to').text('$' + data.to + '.00');
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text('$' + data.from + '.00');
+      $('.filter-price__to').text('$' + data.to + '.00');
+    },
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000
+  });
+
+  $(".filter-recent__star").rateYo({
+    starWidth: "12px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "6px",
+    readOnly: true
   });
 
   var mixer = mixitup('.products__item');
