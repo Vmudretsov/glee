945be7f2e39163b347b7bffa2fb5eb630c58e8eb
Vmudretsov <mr.mudretsov@mail.ru>
2021-08-06 20:59:36 +0700

    added product-details page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,37 @@
 $(function() {
 
+  $('.related-products__items').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/slider/details/arrow-prev.svg" alt="arrow-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/slider/details/arrow-next.svg" alt="arrow-next"></button>',
+    slidesToShow: 4,
+  });
+
+  $('.product-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+
+  $('.product-details__num').styler();
+
+  $('.details-slide__thumb').slick({
+    asNavFor: '.details-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+  });
+  $('.details-slide__big').slick({
+    asNavFor: '.details-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+  
   $('.filter-price__input').ionRangeSlider({
     type: "double",
     prefix: "$",
@@ -26,15 +58,17 @@ $(function() {
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00",
     spacing: "6px",
-    readOnly: true
+    readOnly: true,
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18pt" height="16pt" viewBox="0 0 18 16" version="1.1"><path d="M 8.101562 0.554688 L 6.0625 4.695312 L 1.496094 5.359375 C 0.679688 5.476562 0.351562 6.488281 0.945312 7.066406 L 4.246094 10.285156 L 3.464844 14.832031 C 3.324219 15.652344 4.191406 16.269531 4.914062 15.882812 L 9 13.738281 L 13.085938 15.882812 C 13.808594 16.265625 14.675781 15.652344 14.535156 14.832031 L 13.753906 10.285156 L 17.054688 7.066406 C 17.648438 6.488281 17.320312 5.476562 16.503906 5.359375 L 11.9375 4.695312 L 9.898438 0.554688 C 9.53125 -0.179688 8.472656 -0.191406 8.101562 0.554688 Z M 8.101562 0.554688 " /></svg>',
   });
 
-  $(".product-grid__star").rateYo({
+  $(".star").rateYo({
     starWidth: "18px",
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00",
     spacing: "12px",
-    readOnly: true
+    readOnly: true,
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18pt" height="16pt" viewBox="0 0 18 16" version="1.1"><path d="M 8.101562 0.554688 L 6.0625 4.695312 L 1.496094 5.359375 C 0.679688 5.476562 0.351562 6.488281 0.945312 7.066406 L 4.246094 10.285156 L 3.464844 14.832031 C 3.324219 15.652344 4.191406 16.269531 4.914062 15.882812 L 9 13.738281 L 13.085938 15.882812 C 13.808594 16.265625 14.675781 15.652344 14.535156 14.832031 L 13.753906 10.285156 L 17.054688 7.066406 C 17.648438 6.488281 17.320312 5.476562 16.503906 5.359375 L 11.9375 4.695312 L 9.898438 0.554688 C 9.53125 -0.179688 8.472656 -0.191406 8.101562 0.554688 Z M 8.101562 0.554688 " /></svg>',
   });
 
   var mixer = mixitup('.products__item');
